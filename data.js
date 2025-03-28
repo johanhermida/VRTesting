0e57caad69f869931a9e7d23458ef5730b61282c
johanhermida <johanhermidab@gmail.com>
2025-03-27 22:51:30 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,102 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-guardshack-21",
+      "name": "gUARDSHACK 21",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2997,
+      "initialViewParameters": {
+        "yaw": -2.834218628137096,
+        "pitch": -0.14588904701336602,
+        "fov": 1.4669021853052546
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8873496455268288,
+          "pitch": 0.034631615723498044,
+          "rotation": 6.283185307179586,
+          "target": "1-300-sideim"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.960797330470877,
+          "pitch": -0.15290310993754552,
+          "title": "Tractor Parking",
+          "text": "Hostlers and Daycabs can be park here."
+        }
+      ]
+    },
+    {
+      "id": "1-300-sideim",
+      "name": "300 SIDEIM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2965.5,
+      "initialViewParameters": {
+        "yaw": -1.606151009052688,
+        "pitch": -0.09201816972850096,
+        "fov": 1.4669021853052546
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9892416806733184,
+          "pitch": 0.05771350847506085,
+          "rotation": 0,
+          "target": "0-guardshack-21"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "MCO5 VIRTUAL YARD test",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
